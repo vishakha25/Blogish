ecd8661d9d1ee1fac03ca9057498b0bf426a7107
vishakha25 <vishakha.gupta.ms@gmail.com>
2015-07-22 11:54:40 -0700

    Login Page created

--- Blogish/src/com/blogish-1/main.py
+++ Blogish/src/com/blogish-1/main.py
@@ -15,118 +15,86 @@
 # limitations under the License.
 #
 import webapp2
-
+import string
+import re
 form="""
-<form method="post" >
-    What is your Birthday?
+    <h1>Signup </h1>
+    <form method="post">
+    <label>Name
+    <input type="text" name="username" value="%(username)s">
+    </label>
+    <div style="color:red">%(nameerror)s</div>
+    
+    <label>Password
+    <input type="password" name="pass1" value="%(pass1)s">
+    </label>
+     <div style="color:red">%(passerror)s</div>
+    
+    <label>Verify Password
+    <input type="password" name="pass2" value="%(pass2)s">
+    </label>
+    <div style="color:red">%(passverifyerror)s</div>
+    
+    <label>Email(optional)
+    <input type="email" name="emailaddress" value="%(emailaddress)s">
+    </label>
     <br>
-    <label>Month
-    <input type="text" name="month" value="%(month)s">
-    </label>
-    
-    <label>Day
-    <input type="text" name="day" value="%(day)s">
-    </label>
-    
-    <label>Year
-    <input type="text" name="year" value="%(year)s">
-    </label>
-    
-    <div style="color:red">%(error)s</div>
-    
-    <br>
-    <br>  
- <input type="submit"> 
-</form> """
+     <input type="submit"> 
+</form>
+
+"""
 
 class MainHandler(webapp2.RequestHandler):
-    #setting default values for write_form
-    def write_form(self,error="",month="",day="",year=""):
-        self.response.out.write(form % {"error":error,
-                                        "month":month,
-                                        "day":day,
-                                        "year":year})
-    
+    def write_form(self,username="",nameerror="",pass1="",passerror="",pass2="",passverifyerror="",emailaddress="",):
+        self.response.out.write(form % {"username":username,
+                                      "pass1":pass1,
+                                      "pass2":pass2,
+                                      "emailaddress":emailaddress,
+                                      "nameerror":nameerror,
+                                      "passerror":passerror,
+                                      "passverifyerror":passverifyerror})  
     def get(self):
-        #self.response.write('<h1>Welcome to Blogish!!!</h1>')
-        self.write_form() 
-        
+        self.write_form()
     def post(self):
+        user_username= self.request.get('username')
+        user_pass1=self.request.get('pass1')
+        user_pass2=self.request.get('pass2')
+        user_emailaddress=self.request.get('emailaddress')
+        def valid_username(username):
+            USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+            if not (USER_RE.match(username)):
+                return "That's not a valid username."
+            else:
+                return ""
+        def valid_password(pw1,pw2):
+            PASS_RE= re.compile(r"^.{3,20}$")
+            if not(PASS_RE.match(pw1) or PASS_RE.match(pw2)):
+                return "That wasn't a valid password."
+            else:
+                return ""
+        def valid_passwordMatch(pw1,pw2):
+            if not(pw1==pw2):
+                return "Your passwords didn't match"  
+            else:
+                return "" 
         
-        #####################
-        # date validation methods
-        months = ['January',
-          'February',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July',
-          'August',
-          'September',
-          'October',
-          'November',
-          'December']
-        month_abbrv=dict((m[:3].lower(),m) for m in months)
-       
-        def valid_month(month):    
-            if month:
-                short_month=month[:3].lower()
-                #return month_abbrv.get(short_month)
-                return True
-        def valid_day(day):
-            if day and day.isdigit():
-                d=int(day)
-                if d>0 and d<32:
-                    return True
-        def valid_year(year):
-            if year and year.isdigit():
-                y=int(year)
-                if y>1900 and y<2020:
-                    return True
-                
-        # checking for special characters        
-        def escape_html(s):
-            for(i,o) in (("&", "&amp;"),
-                         (">", "&gt;"),
-                         ("<", "&lt;"),
-                         ('"', "&quot;")):
-                s=s.replace(i,o)
-            return s
-                
-        ###########################
+        nameerror=valid_username(user_username)
+        passerror=valid_password(user_pass1,user_pass2)
+        passverifyerror=valid_passwordMatch(user_pass1, user_pass2)
         
-    
-        user_month= self.request.get('month')
-        user_day= self.request.get('day')
-        user_year= self.request.get('year')
+        if(nameerror or passerror or passverifyerror):
+            self.write_form(user_username, nameerror, user_pass1, passerror, user_pass2, passverifyerror, user_emailaddress)
+        else:    
+            self.redirect("/welcome?username=" + user_username)
+            
+class WelcomeHandler(webapp2.RequestHandler): 
+    def get(self):
+        name=self.request.get('username')
+        self.response.out.write("Welcome, %s!" % name )
+ 
         
-        month= valid_month(user_month)
-        day=valid_day(user_day)
-        year=valid_year(user_year)
-        
-        
-        if not(month and day and year):
-            self.write_form("This is invalid, please enter again",user_month,user_day,user_year)
-        else:
-            self.response.out.write("Validation Passed")          
-           
-
-    
-
-
-
-
-
-'''
-class TestHandler(webapp2.RequestHandler):
-    def post(self):
-        #q=self.request.get("q")
-        #self.response.write(q)  
-        self.response.headers['Content-Type']='text/plain'
-        self.response.out.write(self.request)
-'''             
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    #('/testform',TestHandler)
+    ('/welcome',WelcomeHandler)
 ], debug=True)
+
