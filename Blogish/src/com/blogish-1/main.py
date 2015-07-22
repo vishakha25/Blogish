90ac74be5915b0a4d9c2f2d66e40d15891afd3be
vishakha25 <vishakha.gupta.ms@gmail.com>
2015-07-22 12:05:37 -0700

    Updated all handlers

--- Blogish/src/com/blogish-1/main.py
+++ Blogish/src/com/blogish-1/main.py
@@ -44,7 +44,31 @@ form="""
 
 """
 
-class MainHandler(webapp2.RequestHandler):
+form1="""
+<form method="post" >
+    What is your Birthday?
+    <br>
+    <label>Month
+    <input type="text" name="month" value="%(month)s">
+    </label>
+    
+    <label>Day
+    <input type="text" name="day" value="%(day)s">
+    </label>
+    
+    <label>Year
+    <input type="text" name="year" value="%(year)s">
+    </label>
+    
+    <div style="color:red">%(error)s</div>
+    
+    <br>
+    <br>  
+ <input type="submit"> 
+</form> """
+
+
+class SignupHandler(webapp2.RequestHandler):
     def write_form(self,username="",nameerror="",pass1="",passerror="",pass2="",passverifyerror="",emailaddress="",):
         self.response.out.write(form % {"username":username,
                                       "pass1":pass1,
@@ -85,16 +109,102 @@ class MainHandler(webapp2.RequestHandler):
         if(nameerror or passerror or passverifyerror):
             self.write_form(user_username, nameerror, user_pass1, passerror, user_pass2, passverifyerror, user_emailaddress)
         else:    
-            self.redirect("/welcome?username=" + user_username)
+            self.redirect("/main/welcome?username=" + user_username)
             
 class WelcomeHandler(webapp2.RequestHandler): 
     def get(self):
         name=self.request.get('username')
         self.response.out.write("Welcome, %s!" % name )
+
+class DateHandler(webapp2.RequestHandler):
+    
+    #setting default values for write_form
+    def write_form(self,error="",month="",day="",year=""):
+     # checking for special characters        
+        def escape_html(s):
+            for(i,o) in (("&", "&amp;"),
+                         (">", "&gt;"),
+                         ("<", "&lt;"),
+                         ('"', "&quot;")):
+                s=s.replace(i,o)
+            return s
+        self.response.out.write(form1 % {"error":error,
+                                        "month":escape_html(month),
+                                        "day":escape_html(day),
+                                        "year":escape_html(year)})
+    def get(self):
+        #self.response.write('<h1>Welcome to Blogish!!!</h1>')
+        self.write_form() 
+        
+    def post(self):
+        
+        #####################
+        # date validation methods
+        months = ['January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December']
+        month_abbrv=dict((m[:3].lower(),m) for m in months)
+       
+        def valid_month(month):    
+            if month:
+                short_month=month[:3].lower()
+                #return month_abbrv.get(short_month)
+                return True
+        def valid_day(day):
+            if day and day.isdigit():
+                d=int(day)
+                if d>0 and d<32:
+                    return True
+        def valid_year(year):
+            if year and year.isdigit():
+                y=int(year)
+                if y>1900 and y<2020:
+                    return True
+    
+                
+        ###########################
+        
+    
+        user_month= self.request.get('month')
+        user_day= self.request.get('day')
+        user_year= self.request.get('year')
+        
+        month= valid_month(user_month)
+        day=valid_day(user_day)
+        year=valid_year(user_year)
+        
+        
+        if not(month and day and year):
+            self.write_form("This is invalid, please enter again",user_month,user_day,user_year)
+        else:
+            self.redirect("/thanks")    
+
+class ThanksHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write("Validation Passed")
+
+
+class TestHandler(webapp2.RequestHandler):
+    def post(self):
+        #q=self.request.get("q")
+        #self.response.write(q)  
+        self.response.headers['Content-Type']='text/plain'
+        self.response.out.write(self.request)           
  
         
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/welcome',WelcomeHandler)
+    ('/signup', SignupHandler),
+    ('/main/welcome',WelcomeHandler),
+    ('/birthday',DateHandler),
+    ('/thanks',ThanksHandler)
 ], debug=True)
 
